fcaff38373d2825853a02296c331e8468ecfc6fa
guy <gnzeribe@gmail.com>
2017-07-25 09:03:26 -0400

    first commit

---
+++ chalk_moment.js
@@ -0,0 +1,44 @@
+const chalk = require('chalk');
+const moment = require ('moment');
+// moment().format();
+
+let num1 = moment()
+let num2 = moment()
+let num3 = moment()
+
+function seconds(num1,num2,num3,num4) {
+  result = ((num1 * 60) + (num2 * 60) + num3);
+  return result;
+}
+
+edit
+//moment().add(Number, String);
+//moment().add(Duration);
+//moment().add(Object);
+
+//var moment = require('moment');
+moment().format();
+
+//secondsInday =
+//let = secondsInday
+// constant log = console.log
+// define(['moment'], function (moment) {
+//     console.log(moment().format('LLLL'));
+// });
+
+    function dst() {
+  if (moment()is dst() === (true) {
+    console.log ("is");
+    else{
+      console.log ("is not")
+    }
+
+let timeDetails = 'data';
+
+// console.log(`It is ${timeDetails}`);
+
+console.log(`It is ${chalk.red(moment().format("dddd, Mo Do YYYY, hh:mm a."))}`);
+console.log(moment().format("DDDo"));
+console.log(moment().format("sss"));
+console.log(`It is ${(moment().format())}`)`;
+dst():
